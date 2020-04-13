3f5a119ba1a245b03f732d89bce0f91e4b1c4463
Xinai <xinai.guo@siemens.com>
2020-04-13 17:52:44 +0800

    fix date-fns parse issue

--- src/app/modules/taskmgr/util/date.util.ts
+++ src/app/modules/taskmgr/util/date.util.ts
@@ -1,6 +1,6 @@
 import { isFuture, isDate, isValid, format, differenceInYears, parse } from 'date-fns';
 export const isValidDate = (dateStr) => {
-  const date = parse(dateStr, 'yyyy-MM-dd', new Date());
+  const date = parse(dateStr, 'M/d/yyyy', new Date());
   return isDate(date) && isValid(date) && !isFuture(date) && differenceInYears(Date.now(), date) < 150;
 };
 
